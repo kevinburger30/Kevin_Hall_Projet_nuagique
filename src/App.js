6c5030cde256044e4f528af8fa97e411a0e94dd9
kevinburger30 <75225138+kevinburger30@users.noreply.github.com>
2025-03-21 16:29:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 function App() {
+  async function list() {
+  const endpoint = '/data-api/rest/Person';
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  console.table(data.value);
+}
+
   return     <div><h1>Static Web Apps Database Connections</h1>
   <div>
       <button id="list" onclick="list()">List</button>
