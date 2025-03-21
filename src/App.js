c0e533d8f805601e398b0b3f64e82dd9ab9d2fe9
kevinburger30 <75225138+kevinburger30@users.noreply.github.com>
2025-03-21 16:37:09 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import React from 'react';
 
 function App() {
   async function list() {
-  const endpoint = '/data-api/rest/Produit';
-  const response = await fetch(endpoint);
-  const data = await response.json();
-  console.table(data.value);
-}
-
+    const endpoint = '/data-api/rest/Produit';
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.table(data.value);
+  }
   return     <div><h1>Static Web Apps Database Connections</h1>
   <div>
-      <button id="list" onclick="list()">List</button>
+      <button id="list" onClick={list}>List</button>
       <button id="get" onclick="get()">Get</button>
       <button id="update" onclick="update()">Update</button>
       <button id="create" onclick="create()">Create</button>
       <button id="delete" onclick="del()">Delete</button>
   </div>
+  <script>
+    
+
+</script>
   </div>;
 }
 
