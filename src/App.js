49b5f7b197d1470c531fbf79d27f5edc7b1475cc
kevinburger30 <kevinburger2000@gmail.com>
2025-03-25 14:34:15 -0400

    Commit made from VS Code Azure Static Web Apps

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import { Routes, Route } from 'react-router-dom'
-
-import Connexion from './Connexion'
-import Produits from './Produits';
-
-
-
+import React from 'react';
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Connexion/>}/>
-        <Route path="/Produits" element={<Produits/>}/>
-      </Routes>
-    </>
-  );
+  async function Submit(event) {
+
+    if (event) {
+        event.preventDefault();   const id = 1;
+  const endpoint = `/data-api/rest/Vendeur/VendeurID`;
+  const response = await fetch(`${endpoint}/${id}`);
+  const result = await response.json();
+  if(result.value[0].nomVendeur==document.getElementById("nom").value && 
+  result.value[0].telephone==document.getElementById("telephone").value && 
+  result.value[0].email==document.getElementById("courriel").value && 
+  result.value[0].password==getElementById("password").value){
+    document.location.href = "Produits";
+  }
+  else{
+    document.getElementById("hidden").style.display="block";
+  }
+  }
+}
+  return     <div><h1>Static Web Apps Database Connections</h1>
+  <div>
+      <form onSubmit={Submit}>
+            <input type="text" id="nom" class="input" placeholder="Nom"/>
+            <input type="text" id="telephone" class="input" placeholder="Telephone"/>
+            <input type="text" id="courriel" class="input" placeholder="Courriel"/>
+            <input type="text" id="password" class="input" placeholder="Mot de passe"/>
+                        <button id="submit" class="button" color="primary" type="submit">Connexion</button>
+                        
+
+                </form>
+                <a id="hidden">Ce compte n'est pas celui de l'administrateur.</a>
+  </div>
+  <script>
+    
+
+</script>
+  </div>;
 }
 
 export default App;
